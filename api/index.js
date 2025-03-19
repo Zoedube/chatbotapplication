8f30153a07fba093363c7590e629c9c7db54445f
Zoedube <rofhiwazoe11@gmail.com>
2025-03-19 14:12:59 +0200

    frontendurl

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ console.log('GEMINI_API_KEY:', process.env.GEMINI_API_KEY);
 const app = express();
 
 app.use(cors({
-  origin: 'https://chatbotapplication-kls4-e9wk2627m-zoedubes-projects.vercel.app',
+  origin: 'https://chatbotapplication-kls4.vercel.app/',
   credentials: true,
 }));
 
