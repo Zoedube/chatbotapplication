617b96e67e34314293cd02e659a1bf6e55267fdc
Zoedube <rofhiwazoe11@gmail.com>
2025-03-25 15:13:14 +0200

    final commits

--- api/db.js
+++ api/db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: "ballast.proxy.rlwy.net",
-  user: "root",
-  port: "52150",
-  password:"izpucMSXUjipNxedbWXOmSOOPRSYveyL",
-  database: "railway",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
@@ -13,6 +16,6 @@ db.connect((err) => {
     console.error("Database connection failed:", err);
     return;
   }
-  console.log("Connected to database successfully!");
+  console.log("Connected to Railway MySQL database");
 });
 
